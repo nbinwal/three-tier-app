88a39e5604d2b7defb24aadefed07e61fafa4a2a
nbinwal <95609120+nbinwal@users.noreply.github.com>
2025-05-03 14:26:08 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -9,12 +9,11 @@ output "sql_instance_name" {
 }
 
 output "secret_manager_password_secret" {
-  value       = google_secret_manager_secret.db_password.name
-  description = "The Secret Manager secret resource for the MySQL password"
+  value       = var.database_type == "mysql" ? google_secret_manager_secret.db_password[0].name : null
+  description = "The Secret Manager secret resource for the MySQL password (only available when database_type is mysql)"
 }
 
 output "in_console_tutorial_url" {
   value       = "https://console.cloud.google.com/products/solutions/deployments?walkthrough_id=panels--sic--three-tier-web-app&project=${var.project_id}"
   description = "Console URL to launch the Three Tier App tutorial"
 }
-
