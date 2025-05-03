779f17e55b1957f411b094399db55f4f124e1743
nbinwal <95609120+nbinwal@users.noreply.github.com>
2025-05-03 16:41:53 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,21 @@
+# Outputs: These values will be displayed after `terraform apply` or can be referenced by other configurations.
+
 output "endpoint" {
+  description = "The URL of the deployed frontend (Cloud Run service 'fe')"
   value       = google_cloud_run_service.fe.status[0].url
-  description = "The URL of the front end service"
 }
 
 output "sql_instance_name" {
+  description = "The name of the Cloud SQL instance created"
   value       = google_sql_database_instance.main.name
-  description = "The name of the Cloud SQL instance"
 }
 
 output "secret_manager_password_secret" {
-  value       = var.database_type == "mysql" ? google_secret_manager_secret.db_password[0].name : null
-  description = "The Secret Manager secret resource for the MySQL password (only available when database_type is mysql)"
+  description = "The resource name of the Secret Manager secret holding the DB password"
+  value       = google_secret_manager_secret.db_password.name
 }
 
 output "in_console_tutorial_url" {
+  description = "Link to the GCP Cloud console walkthrough/tutorial for deploying the three-tier app"
   value       = "https://console.cloud.google.com/products/solutions/deployments?walkthrough_id=panels--sic--three-tier-web-app&project=${var.project_id}"
-  description = "Console URL to launch the Three Tier App tutorial"
 }
